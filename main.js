1a49945baa63186dba1ef01c6ef472d9cfd61e6f
Krzysztof Bartkiewicz <krzysiekbar1@gmail.com>
2023-03-19 23:44:41 +0100

    html and css are ready, not responsive yet

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-const API_LINK = 'http://api.openweathermap.org/data/2.5/weather?lat=50&lon=50&air_pollution?lat=50&lon=50'
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
 const API_KEY = '&appid=dca2cbeccdf9e75225ebe67b5f301f72'
 const API_UNITS = '&units=metric'
 const city = 'Kraków'
-const URL = API_LINK + API_KEY + API_UNITS
+const URL = API_LINK + city + API_KEY + API_UNITS
 
 axios.get(URL).then(res => {
 	console.log(res.data)
 })
+
+const time = () => {
+	setInterval(() => {
+		let unix = 1679188658
+		let newTime = new Date(unix * 1000)
+		console.log(newTime)
+	}, 100000000000)
+}
+
+time()
